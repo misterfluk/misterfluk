5385575f7c181a48695e2eba51371ef2ae19b95f
misterfluk <44692806+misterfluk@users.noreply.github.com>
2018-11-02 12:45:43 +0700

    mmm

---
+++ Models/TCustomer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PeTest.Models
+{
+    public partial class TCustomer
+    {
+        public string CustId { get; set; }
+        public string InitialCode { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public string CustType { get; set; }
+
+        public TType CustTypeNavigation { get; set; }
+        public TTitel InitialCodeNavigation { get; set; }
+    }
+}
---
+++ Models/TUnit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PeTest.Models
+{
+    public partial class TUnit
+    {
+        public TUnit()
+        {
+            TProduct = new HashSet<TProduct>();
+        }
+
+        public string UnitCode { get; set; }
+        public string NameUnit { get; set; }
+
+        public ICollection<TProduct> TProduct { get; set; }
+    }
+}
